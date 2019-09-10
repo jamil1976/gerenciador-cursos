8c8447f9bd06186a0a8bf55bebbf752cd27daf84
Jamil Vieira <jamilabuyagui@gmail.com>
2019-09-10 14:49:37 -0300

    Criação das Views

--- public/index.php
+++ public/index.php
@@ -18,3 +18,9 @@ switch ($_SERVER['PATH_INFO']) {
         echo "Erro 404";
         break;
 }
+
+/**
+ * Temos um front controller, por onde todas requisições passam atraves de url's amigaveis
+ * O front chama o controler correspondente a rota solicitada e este controler
+ * busca as informações no model(ENTITY) e retorno para o cliente o resultado(view ou API REST-xml/json).
+ */
